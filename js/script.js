f47e7f05dbc6a62162acaae930c8bb4d2fcdd1d9
NeoDalmatian <aviskovic100@gmail.com>
2022-08-13 16:18:55 +0200

    Implement functionality for operator buttons

--- js/script.js
+++ js/script.js
@@ -1,30 +1,50 @@
-const numbers = document.querySelectorAll(".numbers");
+const numberButtons = document.querySelectorAll(".numbers");
+const operatorButtons = document.querySelectorAll(".blue");
 const display = document.querySelector("#display");
+const equals = document.querySelector("#equals");
 
-let displayMemory= '';
+let firstNumber= "";
+let secondNumber= "";
+let displayOperator = "";
+let displayResult = "";
 
-numbers.forEach(number => {
+numberButtons.forEach(number => {
   number.addEventListener("click", (e) => {
-    displayMemory += e.target.attributes.id.value.toString();
-    populateDisplay();
+    if (displayOperator === "") {
+      firstNumber += e.target.attributes.id.value.toString();
+    } else {
+      secondNumber += e.target.attributes.id.value.toString();
+    }
+    //populateDisplay();
   });
+});
+
+operatorButtons.forEach(operatorButton => {
+  operatorButton.addEventListener("click", (e) => {
+    displayOperator = e.target.attributes.id.value;
+  });
+});
+
+equals.addEventListener("click", () => {
+  operate();
+  console.log(displayResult);
 });
 
 function populateDisplay() {
   const displayElements = document.createElement("div");
-  displayElements.innerText = `${displayMemory}`;
+  displayElements.innerText = `${firstNumber}`;
   display.firstChild.remove();
   display.appendChild(displayElements);
 }
 
 let operators = {
-  add(a, b) {console.log(a + b);} ,
-  subtract(a, b) {console.log (a - b);},
-  multiply(a, b) {console.log(a * b);},
-  divide(a, b) {console.log(a / b);},
+  add(a, b) {displayResult = parseInt(a) + parseInt(b);},
+  subtract(a, b) {displayResult = a - b;},
+  multiply(a, b) {displayResult = a * b;},
+  divide(a, b) {displayResult = a / b;},
 }
 
-function operate (operator, a, b) {
+function operate (operator = displayOperator, a = firstNumber, b = secondNumber) {
   switch (operator) {
     case "+":
       operators.add(a, b);
