f2bbcc587a71ba2b7786c91b8147153f179e1561
NeoDalmatian <aviskovic100@gmail.com>
2022-08-13 11:54:14 +0200

    Implement function populateDisplay

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
+const numbers = document.querySelectorAll(".numbers");
+const display = document.querySelector("#display");
+
+let displayMemory= '';
+
+numbers.forEach(number => {
+  number.addEventListener("click", (e) => {
+    displayMemory += e.target.attributes.id.value.toString();
+    populateDisplay();
+  });
+});
+
+function populateDisplay() {
+  const displayElements = document.createElement("div");
+  displayElements.innerText = `${displayMemory}`;
+  display.firstChild.remove();
+  display.appendChild(displayElements);
+}
+
 let operators = {
   add(a, b) {console.log(a + b);} ,
   subtract(a, b) {console.log (a - b);},
